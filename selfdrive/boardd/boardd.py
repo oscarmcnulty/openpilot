90de770f4b699db5a5e21474f9d40a1a115fd7c9
om <ojfmcnulty@gmail.com>
2023-11-30 07:16:11 +0000

    rebase boardd files

--- selfdrive/boardd/boardd.py
+++ selfdrive/boardd/boardd.py
@@ -1,8 +1,5 @@
-# hard-forked from https://github.com/commaai/openpilot/tree/05b37552f3a38f914af41f44ccc7c633ad152a15/selfdrive/boardd/boardd.py
-# pylint: skip-file
-
 # Cython, now uses scons to build
-from selfdrive.boardd.boardd_api_impl import can_list_to_can_capnp
+from openpilot.selfdrive.boardd.boardd_api_impl import can_list_to_can_capnp
 assert can_list_to_can_capnp
 
 def can_capnp_to_can_list(can, src_filter=None):
