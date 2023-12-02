48c4cdcbddcdf46c641b5f44b9b59c938ed25d5a
om <ojfmcnulty@gmail.com>
2023-12-02 06:37:02 +0000

    add param

--- common/params.cc
+++ common/params.cc
@@ -95,6 +95,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"CalibrationParams", PERSISTENT},
     {"CameraDebugExpGain", CLEAR_ON_MANAGER_START},
     {"CameraDebugExpTime", CLEAR_ON_MANAGER_START},
+    {"CameraMatrix", PERSISTENT}, // only added for flowpilot
     {"CarBatteryCapacity", PERSISTENT},
     {"CarParams", CLEAR_ON_MANAGER_START | CLEAR_ON_ONROAD_TRANSITION},
     {"CarParamsCache", CLEAR_ON_MANAGER_START},
