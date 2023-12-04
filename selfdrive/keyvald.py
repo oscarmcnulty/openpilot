801343a82b1f234beeeea4c9615e224f8a9522c3
om <ojfmcnulty@gmail.com>
2023-12-04 04:26:57 +0000

    refactor keyvald

--- selfdrive/keyvald.py
+++ selfdrive/keyvald.py
@@ -4,24 +4,7 @@ from cereal.messaging.utils import get_zmq_socket_path
 from common.params import Params
 from system.swaglog import cloudlog
 
-
-ctx = zmq.Context()
-sock_get = ctx.socket(zmq.REP)
-sock_get_path = get_zmq_socket_path("6001")
-cloudlog.info("sock_get_path: %s", sock_get_path)
-sock_get.bind(sock_get_path) # get socket
-
-sock_put = ctx.socket(zmq.REP)
-sock_put_path = get_zmq_socket_path("6002")
-cloudlog.info("sock_put_path: %s", sock_put_path)
-sock_put.bind(sock_put_path) # put socket
-
-sock_del = ctx.socket(zmq.REP)
-sock_del_path = get_zmq_socket_path("6003")
-cloudlog.info("sock_del_path: %s", sock_del_path)
-sock_del.bind(sock_del_path) # del socket
-
-params = Params()
+sock_get, sock_put, sock_del = None, None, None
 
 class ParamsServer:
     exit_event = threading.Event()
@@ -32,14 +15,16 @@ class ParamsServer:
         while not exit_event.is_set():
             key, val = sock_put.recv_multipart()
             cloudlog.info(f"keyvald SET: {key} = {val}")
-            params.put(key, val)
+            Params().put(key, val)
             sock_put.send(b"1")
         
     @staticmethod
     def get_thread(exit_event):
         while not exit_event.is_set():
             key = sock_get.recv()
-            data = params.get(key)
+            print(f"keyvald GET: {key}")
+            data = Params().get(key)
+            print(f"keyvald GET: {key} = {data}")
             cloudlog.info(f"keyvald GET: {key} = {data}")
             sock_get.send(data if data is not None else b"")
 
@@ -47,11 +32,26 @@ class ParamsServer:
     def delete_thread(exit_event):
         while not exit_event.is_set():
             key = sock_del.recv()
-            params.remove(key)
+            Params().remove(key)
             sock_del.send(b"1")
     
     @staticmethod 
     def start():
+        ctx = zmq.Context()
+        sock_get = ctx.socket(zmq.REP)
+        sock_get_path = get_zmq_socket_path("6001")
+        cloudlog.info("sock_get_path: %s", sock_get_path)
+        sock_get.bind(sock_get_path) # get socket
+
+        sock_put = ctx.socket(zmq.REP)
+        sock_put_path = get_zmq_socket_path("6002")
+        cloudlog.info("sock_put_path: %s", sock_put_path)
+        sock_put.bind(sock_put_path) # put socket
+
+        sock_del = ctx.socket(zmq.REP)
+        sock_del_path = get_zmq_socket_path("6003")
+        cloudlog.info("sock_del_path: %s", sock_del_path)
+        sock_del.bind(sock_del_path) # del socket
         ParamsServer.exit_event.clear()
         if ParamsServer.threads:
             return
