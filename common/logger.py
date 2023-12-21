23a32585cbceee1fb4293d54ae1b4b898d903853
om <ojfmcnulty@gmail.com>
2023-12-21 07:30:48 +0000

    add debug logger

--- common/logger.py
+++ common/logger.py
@@ -1,5 +1,15 @@
 import logging
 import sys
+import socket
+
+class SocketLogger:
+    def __init__(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def Send(self, log):
+        self.s.sendto((log + "\n").encode(), ("127.0.0.1", 9000))
+
+sLogger = SocketLogger()
 
 def get_logger(name, file_name=None, level=logging.DEBUG):
     logger = logging.getLogger(name)
